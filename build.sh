ba5dc404367217016ad7dd5ed844455d3afa9900
Shikhar <krtonia@gmail.com>
2025-07-18 12:06:17 +0530

    buildscript: Adapt for Infinity-X blossom

--- build.sh
+++ build.sh
@@ -1,26 +1,23 @@
 # Removals
-rm -rf device/xiaomi/beryllium
+rm -rf device/xiaomi/blossom
 
 # Initialize repo with specified manifest
-repo init -u https://github.com/RisingTechOSS/android -b fifteen --git-lfs --depth=1
+repo init --depth=1 --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b 15 -g default,-mips,-darwin,-notdefault
 
 # Clone device tree
-git clone https://github.com/Legendleo90/device_xiaomi_beryllium -b rising device/xiaomi/beryllium
+git clone https://github.com/Voltage-Beryllium/dt_blossom.git -b inf device/xiaomi/blossom
 
 # Sync the repositories
 /opt/crave/resync.sh && /opt/crave/resync.sh
-
-# Private Keys
-rm -rf vendor/lineage-priv && git clone https://github.com/Legendleo90/vendor_lineage-priv vendor/lineage-priv
 
 # Set up build environment
 source build/envsetup.sh
 
 # Lunch configuration
-riseup beryllium user
+lunch infinity_blossom-userdebug
 
 # Git-lfs
 repo forall -c 'git lfs install && git lfs pull && git lfs checkout'
 
 # Build
-rise b
+mka bacon
